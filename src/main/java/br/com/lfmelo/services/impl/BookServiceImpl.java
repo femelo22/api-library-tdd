448e3741c3413e5bd9917410a7e896e4956f6859
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2022-12-15 14:43:10 -0300

    test: teste service para lancamento de excecao

--- src/main/java/br/com/lfmelo/services/impl/BookServiceImpl.java
+++ src/main/java/br/com/lfmelo/services/impl/BookServiceImpl.java
@@ -2,12 +2,17 @@ package br.com.lfmelo.services.impl;
 
 import br.com.lfmelo.entities.Book;
 import br.com.lfmelo.repositories.BookRepository;
+import br.com.lfmelo.resources.exception.BusinessException;
 import br.com.lfmelo.services.BookService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
 
+    /*
+        Não vamos utilizar o Autowired, pois vamos precisar instanciar
+        um Mock do nosso repository no teste
+     */
     private BookRepository repository;
 
     public BookServiceImpl(BookRepository repository) {
@@ -16,6 +21,9 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book save(Book book) {
+        if(repository.existsByIsbn(book.getIsbn())) { //query methods
+            throw new BusinessException("Isbn already registered.");
+        }
         return repository.save(book);
     }
 }
--- src/test/java/br/com/lfmelo/service/BookServiceTest.java
+++ src/test/java/br/com/lfmelo/service/BookServiceTest.java
@@ -2,8 +2,10 @@ package br.com.lfmelo.service;
 
 import br.com.lfmelo.entities.Book;
 import br.com.lfmelo.repositories.BookRepository;
+import br.com.lfmelo.resources.exception.BusinessException;
 import br.com.lfmelo.services.impl.BookServiceImpl;
 import br.com.lfmelo.services.BookService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,16 +36,37 @@ public class BookServiceTest {
     @Test
     @DisplayName("Deve salvar um livro")
     public void saveBookTest() {
+        //cenario
         Book newBook = buildNewBook();
         Book savedBook = buildSavedBook();
+        Mockito.when( repository.existsByIsbn(Mockito.anyString()) ).thenReturn(false);
+        Mockito.when( repository.save(newBook) ).thenReturn(savedBook);
 
-        Mockito.when( repository.save(newBook)).thenReturn(savedBook);
-
+        //execucao
         Book saved = service.save(newBook);
 
+        //validacao
         assertThat(saved.getId()).isNotNull();
         assertThat(saved.getAuthor()).isEqualTo("Autor Teste");
         assertThat(saved.getTitle()).isEqualTo("Titulo Teste");
         assertThat(saved.getIsbn()).isEqualTo("123456");
+    }
+
+    @Test
+    @DisplayName("Deve lancar um erro de negocio ao tentar salvar livro com Isbn já registrado")
+    public void shouldNotSavedBookWithDuplicatedIsbn() {
+        //cenario
+        Book book = buildNewBook();
+        Mockito.when( repository.existsByIsbn( Mockito.anyString()) ).thenReturn(true); //simular que já existe um ISBN cadastrado
+
+        //execucao
+        Throwable exception = Assertions.catchThrowable(() -> service.save(book));
+
+        //verificacao
+        assertThat(exception)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("Isbn already registered.");
+
+        Mockito.verify(repository, Mockito.never()).save(book); //validar que nao chamou o save do repository  ?? existe outras variantes interessantes do Mockito.never()
     }
 }
