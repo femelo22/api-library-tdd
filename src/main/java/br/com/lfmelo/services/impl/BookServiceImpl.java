fc0764e98e8d1ff336bc9971ed020013d517fbe0
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-01-11 16:53:50 -0300

    feat: busca livro por isbn

--- src/main/java/br/com/lfmelo/services/impl/BookServiceImpl.java
+++ src/main/java/br/com/lfmelo/services/impl/BookServiceImpl.java
@@ -76,6 +76,6 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Optional<Book> getBookByIsbn(String isbn) {
-        return Optional.empty();
+        return repository.findByIsbn(isbn);
     }
 }
